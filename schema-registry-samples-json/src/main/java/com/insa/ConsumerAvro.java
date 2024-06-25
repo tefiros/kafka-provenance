bd36c2d906e37978d87f704e6d864e8fa5fe173e
BVivek974 <vivekananda.boudia@gmail.com>
2024-06-25 16:30:22 +0200

    producer and consumer avro/yang

--- schema-registry-samples-json/src/main/java/com/insa/ConsumerAvro.java
+++ schema-registry-samples-json/src/main/java/com/insa/ConsumerAvro.java
@@ -1,36 +1,29 @@
 package com.insa;
 
-import com.insa.kafka.serializers.yang.cbor.KafkaYangCborSchemaDeserializer;
-import com.insa.kafka.serializers.yang.cbor.KafkaYangCborSchemaSerializer;
-import com.insa.kafka.serializers.yang.json.KafkaYangJsonSchemaDeserializer;
-import com.insa.kafka.serializers.yang.json.KafkaYangJsonSchemaSerializer;
-import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.yangcentral.yangkit.data.api.model.YangDataDocument;
-
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 
-public class Consumer {
+public class ConsumerAvro {
     public static void main(String[] args){
         System.out.println("Consumer ->");
 
         Properties consumerConfig = new Properties();
 
-        consumerConfig.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.22.161.64:9092");
+        consumerConfig.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         consumerConfig.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "test");
         consumerConfig.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringDeserializer.class.getName());
-        consumerConfig.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaYangJsonSchemaDeserializer.class.getName());
-        consumerConfig.setProperty("schema.registry.url", "http://172.22.161.64:8081");
+        consumerConfig.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, io.confluent.kafka.serializers.KafkaAvroDeserializer.class.getName());
+        consumerConfig.setProperty("schema.registry.url", "http://localhost:8081");
         consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
 
         KafkaConsumer<Object, Object> consumer = new KafkaConsumer<>(consumerConfig);
-        String topic = "topic.test";
+        String topic = "topic.test-avro";
 
         consumer.subscribe(Collections.singletonList(topic));
 
--- schema-registry-samples-json/src/main/java/com/insa/Producer.java
+++ schema-registry-samples-json/src/main/java/com/insa/Producer.java
@@ -1,125 +1,61 @@
-/*package com.insa;
+package com.insa;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.io.Resources;
-import com.insa.kafka.serializers.yang.KafkaYangSchemaSerializer;
-import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
-import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
-import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
-import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
-import okhttp3.*;
+import com.insa.kafka.serializers.yang.cbor.KafkaYangCborSchemaDeserializer;
+import com.insa.kafka.serializers.yang.json.KafkaYangJsonSchemaSerializer;
+import com.swisscom.kafka.schemaregistry.yang.YangSchema;
+import org.apache.commons.lang3.SerializationException;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.dom4j.DocumentException;
-import org.yangcentral.yangkit.common.api.validate.ValidatorResult;
+import org.yangcentral.yangkit.common.api.validate.ValidatorResultBuilder;
+import org.yangcentral.yangkit.data.api.model.YangDataDocument;
+import org.yangcentral.yangkit.data.codec.json.YangDataParser;
 import org.yangcentral.yangkit.model.api.schema.YangSchemaContext;
 import org.yangcentral.yangkit.parser.YangParserException;
 import org.yangcentral.yangkit.parser.YangYinParser;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
-import static java.util.Arrays.asList;
-
 public class Producer {
-  private static final String TOPIC = "yang.tests";
-  private static final String BOOTSTRAP_SERVERS = "localhost:9092";
-  private static final String SCHEMA_REGISTRY = "http://localhost:8081/";
-  private static OkHttpClient client = new OkHttpClient();
-  public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
-  public static String postSchema(String yangModule) throws IOException {
-    String bodyStart = "{\"schemaType\": \"YANG\", \"schema\": \"";
-    String bodyEnd = "\"}";
-    String escaped = yangModule.replaceAll("\"", "\\\\\"");
-    escaped = escaped.replaceAll("\n", " ");
-    String bodyJson = bodyStart + escaped + bodyEnd;
-    System.out.println("JSON:" + bodyJson);
-    RequestBody body = RequestBody.create(JSON, bodyJson);
-    Request request = new Request.Builder().url(SCHEMA_REGISTRY).post(body).build();
-    Response response = client.newCall(request).execute();
-    if (response.code() != 200){
-      System.out.println("Code = " + response.code());
-      System.out.println(response.body().string());
+    public static void main(String[] args) throws IOException, YangParserException, DocumentException {
+        System.out.println("Producer ->");
+
+        Properties producerConfig = new Properties();
+
+        producerConfig.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        producerConfig.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class.getName());
+        producerConfig.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaYangJsonSchemaSerializer.class.getName());
+        producerConfig.setProperty("schema.registry.url", "http://127.0.0.1:8081");
+
+        KafkaProducer<Object, Object> producer = new KafkaProducer<>(producerConfig);
+
+        YangSchemaContext schemaContext = YangYinParser.parse(Producer.class.getClassLoader().getResource("json/test.yang").getFile());
+        schemaContext.validate();
+        JsonNode jsonNode = new ObjectMapper().readTree(new File(Producer.class.getClassLoader().getResource("json/valid.json").getFile()));
+        ValidatorResultBuilder validatorResultBuilder = new ValidatorResultBuilder();
+        YangDataDocument doc = new YangDataParser(jsonNode, schemaContext, false).parse(validatorResultBuilder);
+        doc.validate();
+
+        String key = "key1";
+        String topic = "topic.test";
+
+        ProducerRecord<Object, Object> record = new ProducerRecord<>(topic, key, doc);
+        try {
+            producer.send(record);
+        } catch (SerializationException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            producer.flush();
+            producer.close();
+        }
+
+        System.out.println("finish !");
+
     }
-    return response.body().string();
-  }
-
-  public static void main(String[] args) throws DocumentException, IOException, YangParserException, RestClientException {
-    //https://github.com/network-analytics/draft-daisy-kafka-yang-integration/blob/main/YANG%20Schema%20registry%20integration.pdf
-
-    // Parsing Yang modules
-    URL yangUrl = Producer.class.getClassLoader().getResource("yang");
-    String yangDir = yangUrl.getFile();
-
-    // TODO: not sure if we need them parsed
-    YangSchemaContext schemaContext = YangYinParser.parse(yangDir);
-    ValidatorResult result = schemaContext.validate();
-    System.out.println("Valid? " + result.isOk());
-    System.out.println("Size modules = " + schemaContext.getModules().size());
-
-    List<String> toRegister = new ArrayList<>(asList(
-            "yang/ietf-inet-types@2021-02-22.yang"
-//        "yang/ietf-yang-types@2023-01-23.yang",
-//        "yang/ietf-restconf@2017-01-26.yang",
-//        "yang/ietf-datastores@2018-02-14.yang"
-//        "yang/insa-test@2023-09-05.yang"
-//        "yang/ietf-yang-structure-ext@2020-06-17.yang"
-//        "yang/ietf-interfaces@2018-02-20.yang"
-//        "yang/ietf-yang-patch@2017-02-22.yang",
-//        "yang/ietf-netconf-acm@2018-02-14.yang",
-//        "yang/ietf-yang-schema-mount@2019-01-14.yang",
-//        "yang/ietf-ip@2018-02-22.yang",
-//        "yang/ietf-network-instance@2019-01-21.yang",
-//        "yang/ietf-notification@2023-07-23.yang",
-//        "yang/ietf-subscribed-notifications@2019-09-09.yang",
-//        "yang/ietf-yang-push@2019-09-09.yang"
-    ));
-    for (String yangModule : toRegister) {
-      URL url = Producer.class.getClassLoader().getResource(yangModule);
-      String yangString = Resources.toString(url, StandardCharsets.UTF_8);
-      postSchema(yangString);
-    }
-
-    SchemaRegistryClient schemaRegistryClient = new CachedSchemaRegistryClient(SCHEMA_REGISTRY, 100);
-    for (String subject : schemaRegistryClient.getAllSubjects()) {
-      System.out.println("Subject: " + subject);
-    }
-
-    InputStream jsonInputStream = Producer.class.getClassLoader().getResourceAsStream("json/valid.json");
-    ObjectMapper objectMapper = new ObjectMapper();
-    JsonNode yangPushNotification = objectMapper.readTree(jsonInputStream);
-
-    // Configure Kafka producer
-    Properties props = new Properties();
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-    props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, SCHEMA_REGISTRY);
-    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaYangSchemaSerializer.class);
-
-    // Create producer
-    KafkaProducer<String, JsonNode> producer = new KafkaProducer<>(props);
-
-    // Send message to Kafka topic
-    ProducerRecord<String, JsonNode> record = new ProducerRecord<>(TOPIC, yangPushNotification);
-
-    producer.send(record, (metadata, exception) -> {
-      if (exception == null) {
-        System.out.println("Message envoyé avec succès. Offset : " + metadata.offset());
-      } else {
-        System.err.println("Erreur lors de l'envoi du message : " + exception.getMessage());
-      }
-    });
-
-    // Fermer le producteur Kafka
-    producer.close();
-  }
-}*/
+}
